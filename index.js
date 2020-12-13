b4ec9cf378dff3ad261f0db5f6c06b7bc29fc52a
Jason Park <qkrckdtls1120@gmail.com>
2020-12-12 22:00:48 -0800

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
+    res.send({ student_number: 'S39482948'})
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 console.log();
